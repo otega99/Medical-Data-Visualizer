aa00ae6e30d1d1349c823f1ad8af1cfbbfef01c3
otega99 <>
2021-06-20 06:58:36 +0000

    my solution

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,25 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # Add 'overweight' column
-df['overweight'] = None
+df['overweight'] = np.where((df['weight']/(df['height']/100)**2)>25,1,0)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+df['cholesterol']=np.where(df['cholesterol']>1,1,0)
+df['gluc']=np.where(df['gluc']>1,1,0)
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = pd.melt(df,id_vars=["cardio"],value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active','overweight']).value_counts()
 
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio','variable','value']).sum().reset_index(name='total')
 
     # Draw the catplot with 'sns.catplot()'
-
+    fig=sns.catplot(x='variable', y= 'total', hue='value', col='cardio', aspect=1, data=df_cat, kind='bar').fig
 
 
     # Do not modify the next two lines
@@ -33,21 +34,25 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[(df['ap_lo']<=df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) &
+      (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025))
+     & (df['weight'] <= df['weight'].quantile(0.975))]
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr().round(1)
 
     # Generate a mask for the upper triangle
-    mask = None
+    sns.set_style("white")
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(16, 13))
+
 
     # Draw the heatmap with 'sns.heatmap()'
-
+    sns.heatmap(corr, mask=mask, annot=True,fmt='.1f')
 
 
     # Do not modify the next two lines
